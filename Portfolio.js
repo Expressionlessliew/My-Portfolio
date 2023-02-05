c881856005fb86c620f599768004ebf9ccdc7a51
Expressionlessliew <xinyi.liew09@gmail.com>
2023-02-05 15:04:08 +0800

    try

--- Portfolio.js
+++ Portfolio.js
@@ -4,7 +4,5 @@ const scrollUp = document.querySelector("#scroll-up");
 scrollUp.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    left: 0,
-    behavior: "smooth",
   });
 });
